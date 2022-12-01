feb2ca9af9bfa309cec803c84bb0f03edccf418e
David Černý <david@cernyd.com>
2022-12-01 10:12:40 +0100

    Day 1 solution - Without part 2

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -6,14 +6,30 @@ use std::io::{self, BufRead};
 fn main() {
     let filename = "calories.txt";
     println!("Using file '{filename}'");
-    let file = File::open("calories.txt").expect("Failed to open file");
+
+    // Setup file read
+    let file = File::open(filename).expect(&format!("Failed to open file '{filename}'").to_string());
     let reader = io::BufReader::new(file);
 
+    let mut max_calories: i64 = 0;
     if let Ok(lines) = Ok::<std::io::Lines<io::BufReader<File>>, String>(reader.lines()) {
+        let mut current_calories: i64 = 0;
+        // Iterate over calorie lines
         for line in lines {
             if let Ok(line_value) = line {
-                println!("{line_value}");
+                // Compare current max with new value, zero current calories
+                if line_value.is_empty() {
+                    if current_calories > max_calories {
+                        max_calories = current_calories;
+                    }
+                    current_calories = 0;
+                // Add calories
+                } else {
+                    current_calories += line_value.parse::<i64>().unwrap();
+                }
             }
         }
     }
+
+    println!("Maximum elf calories: {max_calories} calories");
 }
