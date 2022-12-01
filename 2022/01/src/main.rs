33c3495f5558bae5474b186963f146cf8edce2d5
David Černý <david@cernyd.com>
2022-12-01 11:25:09 +0100

    Day 1 part 2 solution

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -8,10 +8,12 @@ fn main() {
     println!("Using file '{filename}'");
 
     // Setup file read
-    let file = File::open(filename).expect(&format!("Failed to open file '{filename}'").to_string());
+    let file = File::open(filename).expect(
+        &format!("Failed to open file '{filename}'").to_string()
+    );
     let reader = io::BufReader::new(file);
 
-    let mut max_calories: i64 = 0;
+    let mut elf_calories = Vec::<i64>::new();
     if let Ok(lines) = Ok::<std::io::Lines<io::BufReader<File>>, String>(reader.lines()) {
         let mut current_calories: i64 = 0;
         // Iterate over calorie lines
@@ -19,9 +21,7 @@ fn main() {
             if let Ok(line_value) = line {
                 // Compare current max with new value, zero current calories
                 if line_value.is_empty() {
-                    if current_calories > max_calories {
-                        max_calories = current_calories;
-                    }
+                    elf_calories.push(current_calories);
                     current_calories = 0;
                 // Add calories
                 } else {
@@ -31,5 +31,31 @@ fn main() {
         }
     }
 
+    // Get top 3 results
+    let mut top_3 = Vec::<i64>::new();
+    for _ in 0..3 {
+        top_3.push(pop_max(&mut elf_calories));
+    }
+
+    let max_calories = top_3[0];
     println!("Maximum elf calories: {max_calories} calories");
+    let top_3_sum = top_3.iter().sum::<i64>();
+    println!("Top 3 elf calories: {top_3_sum}")
+}
+
+
+// Finds and removes the maximum value from the vector
+fn pop_max(vector: &mut Vec<i64>) -> i64 {
+    let mut max_i: usize = 0;
+    let mut max: i64 = 0;
+
+    for (i, value) in vector.iter().enumerate() {
+        if value > &max {
+            max = *value;
+            max_i = i;
+        }
+    }
+
+    vector.remove(max_i);
+    return max;
 }
