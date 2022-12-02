786739702bd931cdd0fbe4bbc2c0db20b1146843
David Černý <david@cernyd.com>
2022-12-02 12:22:37 +0100

    Day 2 part 2 solution

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::io::{self, BufRead};
 use std::fs::File;
 
@@ -13,6 +14,27 @@ fn read_lines(filename: &str) -> std::io::Lines<io::BufReader<File>> {
     return reader.lines();
 }
 
+fn shape_name(ch: char) -> String {
+    let name = match ch {
+        'A' | 'X' => "Rock",  // Rock
+        'B' | 'Y' => "Paper",  // Paper
+        'C' | 'Z' => "Scissors",  // Scissors
+        _ => panic!("{}", format!("Unknown value {}", ch).to_string())
+    };
+
+    return String::from(name);
+}
+
+fn outcome_name(ch: char) -> String {
+    let name = match ch {
+        'A' | 'X' => "Lose",
+        'B' | 'Y' => "Draw",
+        'C' | 'Z' => "Win",
+        _ => panic!("{}", format!("Unknown value {}", ch).to_string())
+    };
+
+    return String::from(name);
+}
 
 // Returns the score value of a sign
 fn shape_value(ch: char) -> i32 {
@@ -27,32 +49,63 @@ fn shape_value(ch: char) -> i32 {
 // Returns true if value B beats value A
 fn did_win(a: char, b: char) -> bool {
     return match b {
-        'X' =>  // Rock
-            a == 'C',  // Beats scissors
-        'Y' =>  // Paper
-            a == 'A',  // Beats rock
-        'Z' =>  // Scissors
-            a == 'B',  // Beats paper
+        'X' => a == 'C',  // Rock beats scissors
+        'Y' => a == 'A',  // Paper beats rock
+        'Z' => a == 'B',  // Scissors beat paper
         _ => false
     };
 }
 
+fn outcome_shape(a: char, outcome: char) -> char {
+    return match outcome {
+        'X' =>  // To lose
+            match a {
+                'A' => 'Z',  // Rock beats scissors
+                'B' => 'X',  // Paper beats rock
+                'C' => 'Y',  // Scissors beat paper
+                _ => panic!("Unknown symbol")
+            },
+        'Y' =>  // To draw
+            match a {
+                'A' => 'X',
+                'B' => 'Y',
+                'C' => 'Z',
+                _ => panic!("Unknown symbol")
+            }
+        'Z' =>  // To win
+            match a {
+                'A' => 'Y',  // Paper beats rock
+                'B' => 'Z',  // Scissors beat paper
+                'C' => 'X',  // Rock beats scissors
+                _ => panic!("Unknown symbol")
+            }
+        _ => panic!("Unknown symbol")
+    };
+}
 
 fn is_draw(a: char, b: char) -> bool {
     return (a == 'A' && b == 'X') || (a == 'B' && b == 'Y') || (a == 'C' && b == 'Z');
 }
 
-
-fn round_score(line: &String) -> i32 {
+// If target_outcome = false, the second character is treated as the symbol that needs to be played
+// If target_outcome = true, the second character is treated as the outcome that needs to happen
+fn round_score(line: &String, target_outcome: bool) -> i32 {
     assert!(line.len() >= 2);
     let a = line.chars().nth(0).unwrap();
-    let b = line.chars().nth(2).unwrap();
+    let mut b = line.chars().nth(2).unwrap();
+
+    println!();
+    // Change the second character to get desired round result
+    if target_outcome {
+        println!("\n{a} ({}) -> outcome {b} ({})", shape_name(a), outcome_name(b));
+        b = outcome_shape(a, b);
+    }
 
     let b_value = shape_value(b);
 
     let mut score = b_value;
 
-    println!("\n{a} -> {b}");
+    println!("{a} ({}) -> {b} ({})", shape_name(a), shape_name(b));
     println!("{} -> {}", shape_value(a), shape_value(b));
     if did_win(a, b) {
         score += 6;
@@ -72,7 +125,7 @@ fn main() {
     for (_, line) in read_lines(filename).enumerate() {
         let line_values = line.unwrap();
 
-        let score = round_score(&line_values);
+        let score = round_score(&line_values, true);
         total_score += score;
     }
     println!("Total score: {total_score}");
