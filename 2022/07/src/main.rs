2fd58930024e77b81980c57449e7d07f19c788e7
David Černý <david@cernyd.com>
2022-12-09 17:23:19 +0100

    Day 7 part 1 partially working solution

--- 2022/07/src/main.rs
+++ 2022/07/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::BufReader, io::{BufRead, Lines}, collections::HashMap, rc::Rc, cell::{RefCell}, borrow::{BorrowMut, Borrow}};
+use std::{fs::File, io::BufReader, io::{BufRead, Lines}, collections::HashMap, cell::{RefCell}, rc::Rc};
 
 
 #[derive(Debug)]
@@ -10,21 +10,21 @@ struct DirectoryFile {
 
 #[derive(Debug)]
 struct Directory<'a> {
-    parent: Option<&'a mut Box<Directory<'a>>>,
+    parent: Option<Rc<RefCell<Box<Directory<'a>>>>>,
     name: String,
-    files: Vec<Box<DirectoryFile>>,
-    directories: HashMap::<String, Box<Directory<'a>>>
+    files: Vec<DirectoryFile>,
+    directories: HashMap::<String, Rc<RefCell<Box<Directory<'a>>>>>
 }
 
 
 impl<'a> Directory<'a> {
-    fn new(parent: Option<&'a mut Box<Directory<'a>>>, name: String) -> Directory {
-        return Directory {
+    fn new(parent: Option<Rc<RefCell<Box<Directory<'a>>>>>, name: String) -> Rc<RefCell<Box<Directory>>> {
+        return Rc::new(RefCell::new(Box::new(Directory {
             parent: parent,
             name: name,
             files: Vec::new(),
             directories: HashMap::new()
-        };
+        })));
     }
 }
 
@@ -48,17 +48,29 @@ fn read_lines<T: AsRef<str>>(path: T) -> Lines<BufReader<File>> {
 }
 
 
+fn clone_parent(node: Rc<RefCell<Box<Directory>>>) -> Rc<RefCell<Box<Directory>>> {
+    let parent = &node.borrow().parent;
+
+    match parent {
+        Some(x) => {
+            return x.clone();
+        },
+        None => panic!()
+    };
+}
+
+
 fn main() {
     let mut state = &State::LoadNext;
     let mut jump_to = &State::Seek;
     let mut lines = read_lines("filesys.txt").map(|line| line.unwrap());
 
-    let mut root_dir = Box::new(Directory::new(None, String::from("/")));
-    let mut curdir = &mut root_dir;
+    let mut root_dir = Directory::new(None, String::from("/"));
+    let mut curdir = root_dir.clone();
 
     let mut line: String = String::new();
     loop {
-        println!("{:?}", curdir);
+        // println!("CURDIR: {:?}", curdir.borrow().name);
 
         match state {
             /* ----------------------------- Load next line ----------------------------- */
@@ -76,7 +88,6 @@ fn main() {
             /* ---------------------------- Seek next command --------------------------- */
             State::Seek => {
                 if line.starts_with('$') {
-                    println!("COMMAND: {line}");
                     match line.split(" ").nth(1).unwrap() {
                         "cd" => {
                             state = &State::ChangeDir;
@@ -92,27 +103,23 @@ fn main() {
             /* ---------------------------- Change directory ---------------------------- */
             State::ChangeDir => {
                 let target_dir = line.split(" ").nth(2).unwrap();
-                println!("CD TO '{target_dir}'");
-
                 match target_dir {
                     "/" => {
-                        curdir = &mut root_dir;
+                        curdir = root_dir.clone();
                     },
                     ".." => {
-                        curdir = curdir.parent.as_mut().unwrap();
-                        println!("New curdir: '{}'", curdir.name);
+                        curdir = clone_parent(curdir.clone());
                     },
                     _ => {
-                        println!("CD to other directory '{}'", target_dir);
-                        if !curdir.directories.contains_key(target_dir) {
-                            // TODO: parent reference missing
-                            let new_dir = Box::new(Directory::new(None, String::from(target_dir)));
-                            curdir.directories.insert(
+                        if !curdir.borrow().directories.contains_key(target_dir) {
+                            let new_dir = Directory::new(Some(curdir.clone()), String::from(target_dir));
+
+                            curdir.borrow_mut().directories.insert(
                                 String::from(target_dir), new_dir
                             );
                         }
 
-                        curdir = curdir.directories.get_mut(target_dir).unwrap();
+                        curdir = curdir.clone().borrow_mut().directories[target_dir].clone();
                     }
                 }
 
@@ -129,11 +136,16 @@ fn main() {
                         state = &State::Seek;
                     },
                     '0'..='9' => {
-                        println!("Directory file! {line}");
+                        let filesize = line.split(" ").nth(0).unwrap().parse::<usize>().expect("Failed to parse filesize");
+                        let filename = line.split(" ").nth(1).unwrap();
+
+                        curdir.borrow_mut().files.push(
+                            DirectoryFile { name: String::from(filename), size: filesize }
+                        );
                     },
-                    'd' => {
-                        println!("Directory! {line}");
-                    },
+                    // 'd' => {
+                    //     println!("-> directory! {line}");
+                    // },
                     _ => ()
                 };
             },
