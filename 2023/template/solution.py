e27e2262a96d25fc7ac086fd8c28016fb9fd14d2
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-03 11:35:22 +0100

    improving template

--- 2023/template/solution.py
+++ 2023/template/solution.py
@@ -1,21 +1,64 @@
 # https://adventofcode.com/2023/day/?
 
-solution1 = 0
-solution2 = 0
+from abc import abstractmethod
+import abc
+from pathlib import Path
+from typing import Optional
 
 
-def line_generator():
-    with open("input.txt", "r") as file:
-        yield from map(str.strip, file.readlines())
+class AoCTask(abc.ABC):
+    def __init__(self) -> None:
+        self._input_file: Path = None  # type: ignore
+        self.solution1 = 0
+        self.solution2 = 0
 
+    @property
+    def example_solution1(self) -> Optional[int]:
+        """Define example solution to part 1 here"""
+        return None
 
-for line in line_generator():
-    # PUT YOUR CODE HERE
-    # put solution to part 1 to variable solution1
-    # put solution to part 2 to variable solution2
-    print(line)
+    @property
+    def example_solution2(self) -> Optional[int]:
+        """Define example solution to part 2 here"""
+        return None
 
+    def _get_lines(self):
+        yield from map(str.strip, self._input_file.open("r").readlines())
 
-print("=== SOLUTIONS ===")
-print(f"Solution 1: {solution1}")
-print(f"Solution 2: {solution2}")
+    @abstractmethod
+    def _solution(self):
+        """Define your solution here. Set the solution1 and solution2 vars to submit solution."""
+        pass
+
+    def run(self):
+        print("SOLUTIONS".center(80, '='))
+
+        summary = ""
+        for label, file in ("EXAMPLE DATA", "example_input.txt"), ("ACTUAL DATA", "input.txt"):
+            print(label.center(80, '-'))
+            self._input_file = Path(file).resolve()
+            self._solution()
+            check1 = self.__check_solution("solution 1", self.example_solution1,self.solution1)
+            check2 = self.__check_solution("solution 2", self.example_solution2, self.solution2)
+
+            print(check1)
+            print(check2)
+            print()
+
+            summary += label + ":\n"
+            summary += check1 + "\n"
+            summary += check2 + "\n"
+
+        print("SUMMARY".center(80, "~"))
+        print(summary)
+
+    @staticmethod
+    def __check_solution(label: str, expected: Optional[int], actual: int) -> str:
+        status = "[?]"
+
+        if expected == actual:
+            status = "[OK]"
+        else:
+            status = "[FAIL]"
+
+        return f"{status} {label}: {actual} (expected={expected})"
