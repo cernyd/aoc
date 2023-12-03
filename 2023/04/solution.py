9317008840e58c90bd140e815f56d001615cf90e
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-03 11:45:30 +0100

    AOC-2023: Day 4 preparation

---
+++ 2023/04/solution.py
@@ -0,0 +1,19 @@
+# https://adventofcode.com/2023/day/4
+
+from aoc import AoCTask
+
+
+class AocTaskSolution(AoCTask):
+    @property
+    def example_solution1(self) -> int | None:
+        return None
+
+    @property
+    def example_solution2(self) -> int | None:
+        return None
+
+    def _solution(self):
+        pass
+
+
+AocTaskSolution().run()
