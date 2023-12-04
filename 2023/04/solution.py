4f3656f33513038a99b6ebde5592110200a3d13e
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-04 08:53:13 +0100

    AOC-2023: Day 4 solution 1 attempt 1 - Not working for actual input, just example input

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -6,14 +6,36 @@ from aoc import AoCTask
 class AocTaskSolution(AoCTask):
     @property
     def example_solution1(self) -> int | None:
-        return None
+        return 13
 
     @property
     def example_solution2(self) -> int | None:
         return None
 
     def _solution(self):
-        pass
+        for line in self._get_lines():
+            _, numbers = line.split(":")
+            winning, ours = list(map(self._numbers_to_set, numbers.strip().split("|")))
+            # print(f"WINNING {len(winning)}: {winning} OURS {len(ours)}: {ours}")
+            our_winning = ours.intersection(winning)
+            score = self._get_card_score(our_winning)
+            print(f"CARD {line} SCORE {score} (intersect {our_winning})")
+            self.solution1 += score
+
+    @staticmethod
+    def _numbers_to_set(nums: str) -> set[int]:
+        return set(map(int, nums.split()))
+
+    @staticmethod
+    def _get_card_score(nums: list[int]):
+        if not nums:
+            return 0
+
+        total = 1
+        for _ in range(len(nums)-1):
+            total *= 2
+
+        return total
 
 
 AocTaskSolution().run()
