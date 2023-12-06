be8d67eb76c8513346d09e011a293b4abf0510c9
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-06 09:09:39 +0100

    AOC-2023: Day 6 part 1 solution

--- 2023/06/solution.py
+++ 2023/06/solution.py
@@ -6,7 +6,7 @@ from aoc import AoCTask
 class AocTaskSolution(AoCTask):
     @property
     def example_solution1(self) -> int | None:
-        return None
+        return 288
 
     @property
     def example_solution2(self) -> int | None:
@@ -14,15 +14,42 @@ class AocTaskSolution(AoCTask):
 
     @property
     def actual_solution1(self) -> int | None:
-        return None
+        return 114400
 
     @property
     def actual_solution2(self) -> int | None:
         return None
 
     def _solution(self):
-        pass
+        race_times, race_distances = list(self._get_lines())
+        _, race_times = race_times.split(':')
+        _, race_distances = race_distances.split(':')
 
+        race_times = list(map(int, race_times.split()))
+        race_distances = list(map(int, race_distances.split()))
+
+        self.solution1 = 1
+        for time, distance in zip(race_times, race_distances):
+            combinations = self._get_total_win_combinations(time, distance)
+            print(f"TIME={time}, DISTANCE={distance}, COMBINATIONS={combinations}")
+            self.solution1 *= combinations
+
+        print(race_times)
+        print(race_distances)
+
+    @staticmethod
+    def _get_total_win_combinations(time: int, record_distance: int):
+        combinations: int = 0
+
+        for hold_time in range(1, time):
+            distance = hold_time * (time - hold_time)
+            if distance > record_distance:
+                combinations += 1
+            elif combinations:
+                # If there already are ways to win, we break
+                break
+
+        return combinations
 
 if __name__ == "__main__":
     AocTaskSolution().run()
