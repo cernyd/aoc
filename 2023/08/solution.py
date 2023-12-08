0d309ee1b9f30dd9d18710fd01ee8d69a2cf4e25
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-08 09:45:35 +0100

    AOC-2023: Day 8 part 1 solution - With graphviz disabled

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,12 +1,15 @@
 # https://adventofcode.com/2023/day/8
 
+from itertools import cycle
+import os
 from aoc import AoCTask
+from graphviz import Digraph
 
 
 class AocTaskSolution(AoCTask):
     @property
     def example_solution1(self) -> int | None:
-        return None
+        return 6
 
     @property
     def example_solution2(self) -> int | None:
@@ -21,7 +24,47 @@ class AocTaskSolution(AoCTask):
         return None
 
     def _solution(self):
-        pass
+        lines = self._get_lines()
+
+        instructions = next(lines)
+        print(f"INSTRUCTIONS: {instructions}")
+        next(lines)  # Empty line
+
+        dot = Digraph(comment="Maze")
+        nodes = {}
+        for line in lines:
+            start, ends = line.split("=")
+            start = start.strip()
+            end1, end2 = ends.strip().strip("(").strip(")").split(", ")
+
+            attrs = {"style": "filled", "fillcolor": "white"}
+
+            if start == "AAA":
+                attrs["fillcolor"] = "green"
+
+            if start == "ZZZ":
+                attrs["fillcolor"] = "red"
+
+            nodes[start] = (end1, end2)
+
+            dot.node(start, **attrs)
+            dot.edge(start, end1)
+            dot.edge(start, end2)
+
+        # dot.render("maze.dot")
+        # os.system("dot -Tpng -Kneato -o maze.png maze.dot")
+
+        curr_node: str = "AAA"
+        for instr in cycle(instructions):
+            direction: int = 1 if instr[0] == "R" else 0
+
+            # prev_node = curr_node
+            curr_node = nodes[curr_node][direction]
+            self.solution1 += 1
+            # input(f"{prev_node} -> {curr_node} (instr={instr} sol={self.solution1})")
+
+            if curr_node == "ZZZ":
+                break
 
 
 if __name__ == "__main__":
