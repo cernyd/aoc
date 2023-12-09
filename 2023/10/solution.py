8e91bbd5accc505e72516986ddfcdf0a328d5e9c
David Černý <18537381+cernyd@users.noreply.github.com>
2023-12-09 20:42:15 +0100

    AOC-2023: Day 10 part 1 preparation

---
+++ 2023/10/solution.py
@@ -0,0 +1,28 @@
+# https://adventofcode.com/2023/day/10
+
+from aoc import AoCTask
+
+
+class AocTaskSolution(AoCTask):
+    @property
+    def example_solution1(self) -> int | None:
+        return None
+
+    @property
+    def example_solution2(self) -> int | None:
+        return None
+
+    @property
+    def actual_solution1(self) -> int | None:
+        return None
+
+    @property
+    def actual_solution2(self) -> int | None:
+        return None
+
+    def _solution(self):
+        pass
+
+
+if __name__ == "__main__":
+    AocTaskSolution().run()
